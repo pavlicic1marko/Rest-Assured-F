5defe616abd8007c7b56836a81e61c373fb4b1e3
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-27 12:44:37 +0200

    adding first test

---
+++ src/test/java/com/student/tests/CrudTest.java
@@ -0,0 +1,32 @@
+package com.student.tests;
+
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CrudTest {
+
+    @Test
+    public void test(){
+
+        RestAssured.baseURI ="http://localhost:8000/api/routes";
+
+
+        RestAssured.given()
+                .when()
+                .get()
+                .then()
+                .statusCode(200);
+
+    }
+}
+
+
+
+
+
+
+
