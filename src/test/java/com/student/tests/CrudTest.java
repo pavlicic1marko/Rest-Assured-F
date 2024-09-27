f1b1a36837693efd82022d371ec6bac3b00bbd0b
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-27 21:02:24 +0200

    adding story, display name and feature

--- src/test/java/com/student/tests/CrudTest.java
+++ src/test/java/com/student/tests/CrudTest.java
@@ -1,14 +1,21 @@
 package com.student.tests;
 
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
+@Story("get routes for microservice")
 public class CrudTest {
 
+    @Story("get routes for microservice")
+    @DisplayName("Test Name")
+    @Feature("Feature name")
     @Test
     public void test(){
 
