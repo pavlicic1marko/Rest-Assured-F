abca0e29522a6b568fd1e42377b4fc59f7627837
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-27 12:47:47 +0200

    log response body

--- src/test/java/com/student/tests/CrudTest.java
+++ src/test/java/com/student/tests/CrudTest.java
@@ -19,6 +19,8 @@ public class CrudTest {
                 .when()
                 .get()
                 .then()
+                .log()
+                .body()
                 .statusCode(200);
 
     }
