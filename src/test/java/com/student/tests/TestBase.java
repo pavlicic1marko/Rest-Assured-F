0f911f2685dfb1affd22c39a34c2ccdcffe9a8d6
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-09-30 22:05:33 +0200

    read property from file

--- src/test/java/com/student/tests/TestBase.java
+++ src/test/java/com/student/tests/TestBase.java
@@ -1,14 +1,19 @@
 package com.student.tests;
 
+import com.student.util.PropertyReader;
 import io.restassured.RestAssured;
 import org.junit.Before;
 
 public class TestBase {
 
+    public static PropertyReader prop;
+
+
     @Before
     public void init(){
+        prop = PropertyReader.getInstance();
 
-        RestAssured.baseURI ="http://localhost/api";
-        RestAssured.port = 8000;
+        RestAssured.baseURI =prop.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(prop.getProperty("port"));
     }
 }
