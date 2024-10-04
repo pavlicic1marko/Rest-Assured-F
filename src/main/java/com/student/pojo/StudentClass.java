7be873e745e4d6a3f7f53a85da4fbb8ad5abde0a
MP <pavlicic1marko@gmail.com>
2024-10-04 10:31:02 +0200

    add pojo class

---
+++ src/main/java/com/student/pojo/StudentClass.java
@@ -0,0 +1,53 @@
+package com.student.pojo;
+
+public class StudentClass {
+
+    String firstName;
+    String lastName;
+    String email;
+    String program;
+    String course;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
