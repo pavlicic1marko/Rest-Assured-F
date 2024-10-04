4f410a4521555b4a33f7034f6b99b5e035559cca
MP <pavlicic1marko@gmail.com>
2024-10-04 09:55:00 +0200

    add post request to request factory

--- src/test/java/com/student/requests/RestClient.java
+++ src/test/java/com/student/requests/RestClient.java
@@ -2,6 +2,7 @@ package com.student.requests;
 
 import com.student.tests.TestBase;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class RestClient extends TestBase {
@@ -11,5 +12,14 @@ public class RestClient extends TestBase {
         return RestAssured.given().header("Authorization", "Bearer "+ access_token)
                 .when()
                 .get(requestPath);
+    }
+
+    public Response doPostRequest(String path, Object body){
+
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(body)
+                .post(path);
     }
 }
