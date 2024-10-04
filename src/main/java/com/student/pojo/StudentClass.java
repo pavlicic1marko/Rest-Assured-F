2d337c136544a11c3ffdf9539407b4622b831993
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2024-10-04 11:28:19 +0200

    Student pojo (#12)
    * add pojo class
    
    * add student  pojo to request

---
+++ src/main/java/com/student/pojo/StudentClass.java
@@ -0,0 +1,55 @@
+package com.student.pojo;
+
+import java.util.List;
+
+public class StudentClass {
+
+    String firstName;
+    String lastName;
+    String email;
+    String program;
+    List<String> course;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public List<String> getCourse() {
+        return course;
+    }
+
+    public void setCourse( List<String> course) {
+        this.course = course;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
