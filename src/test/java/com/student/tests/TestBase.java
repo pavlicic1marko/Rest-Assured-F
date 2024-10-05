a6d08b2b4af62a9b048434037034b87c3eb59a4a
MP <pavlicic1marko@gmail.com>
2024-10-05 11:28:09 +0200

    implement test listener for success

--- src/test/java/com/student/tests/TestBase.java
+++ src/test/java/com/student/tests/TestBase.java
@@ -3,12 +3,24 @@ package com.student.tests;
 import com.student.util.PropertyReader;
 import io.restassured.RestAssured;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 public class TestBase {
 
     public static PropertyReader prop;
     public String access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI4NDU5NTQxLCJpYXQiOjE3MjgwMjc1NDEsImp0aSI6ImY3OGY3Mjc0MDU3OTQ5NTY4MGJhZTJlNTZhOTRkOTY2IiwidXNlcl9pZCI6MX0.-iHRIueMLTSTDFCBdGWGwmod80vt-zhCErofgWhAGwA";
 
+    @Rule
+    public TestRule listener = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description){
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test ended successfully");
+        }
+    };
 
     @Before
     public void init(){
