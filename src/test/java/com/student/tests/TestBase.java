e6cb0b1a317ee5120f40eac7b895722ecef10d6f
MP <pavlicic1marko@gmail.com>
2024-10-05 11:40:56 +0200

    implement failed, starting, finish listeners

--- src/test/java/com/student/tests/TestBase.java
+++ src/test/java/com/student/tests/TestBase.java
@@ -16,17 +16,35 @@ public class TestBase {
     @Rule
     public TestRule listener = new TestWatcher() {
         @Override
-        protected void succeeded(Description description){
+        protected void succeeded(Description description) {
             System.out.println("---------------------------------------------------------");
             System.out.println(description.getMethodName() + " test ended successfully");
         }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test ended failed");
+        }
+
+        @Override
+        protected void starting(Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test has started");
+        }
+
+        @Override
+        protected void finished(Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test has ended");
+        }
+
+        @Before
+        public void init() {
+            prop = PropertyReader.getInstance();
+
+            RestAssured.baseURI = prop.getProperty("baseUrl");
+            RestAssured.port = Integer.parseInt(prop.getProperty("port"));
+        }
     };
-
-    @Before
-    public void init(){
-        prop = PropertyReader.getInstance();
-
-        RestAssured.baseURI =prop.getProperty("baseUrl");
-        RestAssured.port = Integer.parseInt(prop.getProperty("port"));
-    }
 }
