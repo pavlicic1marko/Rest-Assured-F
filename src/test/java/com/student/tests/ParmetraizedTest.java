1e11e4974b61a5be22d027cf6cc8cf1505e670f5
MP <pavlicic1marko@gmail.com>
2024-10-05 13:38:19 +0200

    adding parameterized test

---
+++ src/test/java/com/student/tests/ParmetraizedTest.java
@@ -0,0 +1,32 @@
+package com.student.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ParmetraizedTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][] {
+                        {0, 0}, {1, 1}, {2, 1}
+                });
+
+    }
+    private int fInput;
+    private int fExpected;
+
+    public ParmetraizedTest(int input, int expected){
+        this.fInput = input;
+        this.fExpected = expected;
+    }
+
+    @Test
+    public void parameterizedTest(){
+        System.out.println(fInput + " " + fExpected);
+    }
+}
