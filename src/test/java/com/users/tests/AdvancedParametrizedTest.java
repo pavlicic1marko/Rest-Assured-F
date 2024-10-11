42455cddbc97889669b5ba99d17de54bdf2cb94a
MP <pavlicic1marko@gmail.com>
2024-10-11 16:29:47 +0200

    use data provider data to create 3 non admin users

--- src/test/java/com/users/tests/AdvancedParametrizedTest.java
+++ src/test/java/com/users/tests/AdvancedParametrizedTest.java
@@ -1,23 +1,31 @@
 package com.users.tests;
 
+import com.github.javafaker.Faker;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import com.users.requests.RequestFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(DataProviderRunner.class)
-public class AdvancedParametrizedTest {
+public class AdvancedParametrizedTest extends TestBase{
+
+    RequestFactory requestFactory = new RequestFactory();
 
     @DataProvider
-    public static Object[][] dataProviderAdd(){
-      return  new Object[][]{
-              {0,1},{1,2},{3,4}
+    public static Object[][] userEmails(){
+        Faker fakerApi = new Faker();
+
+        return  new Object[][]{
+              {fakerApi.name().fullName(),fakerApi.name().lastName(),fakerApi.name().nameWithMiddle()},
+                {fakerApi.name().fullName(),fakerApi.name().lastName(),fakerApi.name().nameWithMiddle()},
+                {fakerApi.name().fullName(),fakerApi.name().lastName(),fakerApi.name().nameWithMiddle()}
       };
     }
 
     @DataProvider
-    public static Object[][] dataProviderAdd2(){
+    public static Object[][] dataProviderAdd(){
         return  new Object[][]{
                 {110,111},
         };
@@ -25,14 +33,16 @@ public class AdvancedParametrizedTest {
 
     @UseDataProvider("dataProviderAdd")
     @Test
+
     public void firstTest(int num1, int num2){
         System.out.println(num1 +" OR "+ num2);
     }
 
-    @UseDataProvider("dataProviderAdd2")
+    @UseDataProvider("userEmails")
     @Test
-    public void firstTest2(int num1, int num2){
-        System.out.println(num1 +" OR "+ num2);
+    public void registerUsers(String email, String firstName, String password){
+        requestFactory.registerUser(email ,firstName ,password, false).then().log().all();
+        System.out.println(email);
     }
     @Test
     public void secondTest(){
