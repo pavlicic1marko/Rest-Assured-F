22066a68b5de5f25898a2ad81ba2acc5997d8168
MP <pavlicic1marko@gmail.com>
2024-10-11 21:54:59 +0200

    adding download file test

--- src/test/java/com/users/tests/files/FileDownload.java
+++ src/test/java/com/users/tests/files/FileDownload.java
@@ -5,17 +5,26 @@ import org.junit.Test;
 
 import java.io.File;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class FileDownload {
 
     @Test
     public void downloadFileTest(){
 
-        byte[] file = RestAssured.given()
+        File inputFile = new File("src/main/resources/20MB-TESTFILE.ORG.pdf");
+        int expectedFilesize = (int)inputFile.length();
+
+
+
+        byte[] fileDownloaded = RestAssured.given()
                 .header("Accept", "*/*")
                 .header("Accept-Encoding", "gzip, deflate, br")
                 .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
                 .get("https://files.testfile.org/PDF/20MB-TESTFILE.ORG.pdf").then().log().headers().extract().asByteArray();
 
+        assertThat(expectedFilesize,equalTo(fileDownloaded.length));
     }
 
     @Test
