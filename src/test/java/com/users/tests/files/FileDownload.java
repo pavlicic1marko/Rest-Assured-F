886011c8202cde7e6dfc09f7b70c5225930f2436
MP <pavlicic1marko@gmail.com>
2024-10-11 21:24:07 +0200

    adding upload file test

--- src/test/java/com/users/tests/files/FileDownload.java
+++ src/test/java/com/users/tests/files/FileDownload.java
@@ -3,6 +3,8 @@ package com.users.tests.files;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
+import java.io.File;
+
 public class FileDownload {
 
     @Test
@@ -13,6 +15,18 @@ public class FileDownload {
                 .header("Accept-Encoding", "gzip, deflate, br")
                 .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
                 .get("https://files.testfile.org/PDF/20MB-TESTFILE.ORG.pdf").then().log().headers().extract().asByteArray();
+
+    }
+
+    @Test
+    public  void uploadFile(){
+
+        File inputFile = new File("src/main/resources/Transformer.jpg");
+
+        RestAssured.given()
+                .multiPart("image", inputFile)
+                .post("http://127.0.0.1:8000/api/users/images/upload")
+                .then().log().all();
 
     }
 }
