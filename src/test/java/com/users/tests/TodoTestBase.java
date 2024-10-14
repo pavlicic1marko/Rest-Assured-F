e0837a8d4a62c7af82dee8136e9fc43201c63d0a
MP <pavlicic1marko@gmail.com>
2024-10-15 01:23:34 +0200

    refactor

--- src/test/java/com/users/tests/TodoTestBase.java
+++ src/test/java/com/users/tests/TodoTestBase.java
@@ -14,34 +14,6 @@ public class TodoTestBase extends TestBase{
 
 
 
-    @Rule
-    public TestRule listener = new TestWatcher() {
-        @Override
-        protected void succeeded(Description description) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println(description.getMethodName() + " test ended successfully");
-        }
-
-        @Override
-        protected void failed(Throwable e, Description description) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println(description.getMethodName() + " test ended failed");
-        }
-
-        @Override
-        protected void starting(Description description) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println(description.getMethodName() + " test has started");
-        }
-
-        @Override
-        protected void finished(Description description) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println(description.getMethodName() + " test has ended");
-        }
-
-
-    };
 
     @Before
     public void init() {
