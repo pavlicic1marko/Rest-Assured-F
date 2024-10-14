11771e3d7580ca685adfb910aba2d967c6a311a1
MP <pavlicic1marko@gmail.com>
2024-10-14 23:46:58 +0200

    refactor test

--- src/test/java/com/users/tests/TodoService/TodoTest.java
+++ src/test/java/com/users/tests/TodoService/TodoTest.java
@@ -3,6 +3,8 @@ package com.users.tests.TodoService;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.hasKey;
+
 public class TodoTest {
 
     protected String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5MzY2OTA2LCJpYXQiOjE3Mjg5MzQ5MDYsImp0aSI6IjE1ZDc4NTRmYzRlMTQzMDNhOTU0MWMyOGZmN2U0ZmIyIiwidXNlcl9pZCI6MX0.tTUbpxCWfLmAQtPGGt1V8mOEo665jTl1xXdiGYWXHYE";
@@ -24,23 +26,23 @@ public class TodoTest {
 
     @Test
     public void CreateTodoAndDelete(){
-        RestAssured.given()
+        String todoId = RestAssured.given()
                 .header("Content-Type","application/json")
                 .header("Authorization","Bearer " + token)
                 .when()
                 .body("{\n" +
                 "    \"title\":\"create from Rest Assured\",\n" +
                 "    \"description\" :\"created from Rest \"\n" +
-                "}").post("http://127.0.0.1:8001/api/create" ).then().log().all();
+                "}").post("http://127.0.0.1:8001/api/create" ).then()
+                .body("$",hasKey("_id"))
+                .extract().path("_id").toString();
 
         RestAssured.given()
                 .header("Content-Type","application/json")
                 .header("Authorization","Bearer " + token)
                 .when()
-                .delete("http://127.0.0.1:8001/api/delete/61")
+                .delete("http://127.0.0.1:8001/api/delete/" + todoId)
                 .then().log().all();
-
-
 
     }
 }
