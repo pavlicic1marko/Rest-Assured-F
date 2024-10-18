2bd0081244ad5e60ebb4b9f54cfa21f4b787e873
MP <pavlicic1marko@gmail.com>
2024-10-18 21:30:17 +0200

    adding products test, get top, get by id...

--- src/test/java/com/users/tests/products/ProductTests.java
+++ src/test/java/com/users/tests/products/ProductTests.java
@@ -10,6 +10,23 @@ public class ProductTests extends ProdictsBaseClass {
     @Test
     public void getAllProducts(){
         RestAssured.given().get("/products").then().log().all();
+    }
 
+    @Test
+    public void getProductById(){
+        RestAssured.given().get("/products/6/").then().log().all();
+    }
+
+    @Test
+    public void getTopProducts(){
+        RestAssured.given().get("/products/top/").then().log().all();
+    }
+
+    //TODO add comment
+    @Test
+    public void getProductsByKeyword(){
+        String keyword = "mouse";
+        String page= "1";
+        RestAssured.given().get("/products/?keyword=" + keyword + "&page=" +page).then().log().all();
     }
 }
