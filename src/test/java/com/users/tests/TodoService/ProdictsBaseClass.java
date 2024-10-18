3b347d554f5da2156dcc1001ee664c0e6ea1197f
MP <pavlicic1marko@gmail.com>
2024-10-18 17:55:50 +0200

    adding ProductBaseClass and Using it in tests

---
+++ src/test/java/com/users/tests/TodoService/ProdictsBaseClass.java
@@ -0,0 +1,70 @@
+package com.users.tests.TodoService;
+
+import com.users.util.PropertyReader;
+import com.users.util.UserCredentialsReader;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import static org.hamcrest.Matchers.hasKey;
+
+public class ProdictsBaseClass {
+
+
+
+    public static PropertyReader prop;
+    public static UserCredentialsReader credentialsProp;
+    public String access_token = getToken();
+
+    public String getToken(){
+
+        credentialsProp = UserCredentialsReader.getInstance();
+
+
+        return RestAssured.given().header("Content-Type","application/json")
+                    .body("{\"username\":\"pavlicicmarko@yahoo.com\",\"password\":\"Posao2018!\"}")
+                    .when()
+                    .post("http://127.0.0.1:8000/api/users/login/").then().body("$",hasKey("access"))
+                    .extract().path("access");
+
+    }
+    @Rule
+    public TestRule listener = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test ended successfully");
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test ended failed");
+        }
+
+        @Override
+        protected void starting(Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test has started");
+        }
+
+        @Override
+        protected void finished(Description description) {
+            System.out.println("---------------------------------------------------------");
+            System.out.println(description.getMethodName() + " test has ended");
+        }
+
+
+    };
+
+    @Before
+    public void init() {
+        prop = PropertyReader.getInstance();
+
+        RestAssured.baseURI = prop.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(prop.getProperty("port"));
+    }
+}
--- src/test/java/com/users/tests/products/ProductTests.java
+++ src/test/java/com/users/tests/products/ProductTests.java
@@ -1,13 +1,15 @@
 package com.users.tests.products;
 
+import com.users.tests.TodoService.ProdictsBaseClass;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
-public class ProductTests {
+
+public class ProductTests extends ProdictsBaseClass {
 
     @Test
     public void getAllProducts(){
-        RestAssured.given().get("http://127.0.0.1:8000/api/products").then().log().all();
+        RestAssured.given().get("/products").then().log().all();
 
     }
 }
