4143331479c16783630869a9cad08ebc5fddf3ba
MP <pavlicic1marko@gmail.com>
2024-10-23 23:24:13 +0200

    adding Review pojo

---
+++ src/main/java/com/users/pojo/Review.java
@@ -0,0 +1,26 @@
+package com.users.pojo;
+
+public class Review {
+
+    private String rating; //from 0 to 5
+    private String comment;
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+
+}
