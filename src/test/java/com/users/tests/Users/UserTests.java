b803f8bb76c3c55d1c8caa12f20583aed18acf85
MP <pavlicic1marko@gmail.com>
2024-10-28 14:59:36 +0100

    implement user tests

--- src/test/java/com/users/tests/Users/UserTests.java
+++ src/test/java/com/users/tests/Users/UserTests.java
@@ -1,6 +1,9 @@
 package com.users.tests.Users;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import com.users.pojo.User;
 import com.users.requests.RequestFactory;
 import com.users.tests.ProductsBaseClass;
 import org.junit.Test;
@@ -31,26 +34,65 @@ public class UserTests extends ProductsBaseClass {
 
     @Test
     public void getUserProfile(){
+        requestFactory.getUserProfile().then().statusCode(200).log().all();
 
     }
 
     @Test
-    public void getUsers(){
-
+    public void getUsers()
+    {
+        requestFactory.getUsers().then().log().all();
     }
 
     @Test
     public void getUsersById(){
+        String userId = "1";
+        requestFactory.getUsersById(userId).then().log().all();
 
     }
 
+    // update user vs update profile, you can change isAdmin, email, name
     @Test
     public void updateUser(){
+
+
+        //create user
+        String userId =  requestFactory.registerUserEshop().then().extract().path("id").toString();
+        System.out.println("user id is:" + userId);
+
+
+        //update user, email and TODO set isAdmin to True
+        User user = new User();
+        Faker fakerApi = new Faker();
+        String username = fakerApi.name().firstName();
+
+        user.setName(username);
+        user.setEmail(username + "updated@test.com");
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString;
+        try {
+            jsonInString = mapper.writeValueAsString(user);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+
+        }
+        requestFactory.updateUser(jsonInString).then().log().all();
 
     }
 
     @Test
     public void deleteUser(){
+
+        Faker faker = new Faker();
+        String newEmail = faker.name().username() + "@test.com";
+
+        //create user
+        String userId =  requestFactory.registerUserEshop().then().extract().path("id").toString();
+        System.out.println("user id is:" + userId);
+
+        //delete user
+        requestFactory.deleteUser(userId).then().statusCode(200).log().all();
+
 
     }
 }
