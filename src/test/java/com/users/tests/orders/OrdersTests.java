8063038a9d92fe0afb2100e8d71bf10212294e0e
MP <pavlicic1marko@gmail.com>
2024-10-28 19:31:15 +0100

    adding orders test class

---
+++ src/test/java/com/users/tests/orders/OrdersTests.java
@@ -0,0 +1,30 @@
+package com.users.tests.orders;
+
+import org.junit.Test;
+
+public class OrdersTests {
+
+    @Test
+    public void getOrders(){
+    }
+
+    @Test
+    public void getOrdersItems(){
+    }
+
+    @Test
+    public void getMyOrders(){
+    }
+
+    @Test
+    public void updateOrdersToDelivered(){
+    }
+
+    @Test
+    public void getOrderById(){
+    }
+
+    @Test
+    public void updateOrderToPaid(){
+    }
+}
