ce2621a180552e85cd51b1fea1406370afd7583a
MP <pavlicic1marko@gmail.com>
2024-10-29 20:24:54 +0100

    add get admin token method

--- src/test/java/com/users/util/Token.java
+++ src/test/java/com/users/util/Token.java
@@ -22,4 +22,19 @@ public class Token {
                 .body("$",hasKey("access"))
                 .extract().path("access");
     }
+
+    public  String getTokenForAdminUser(){
+
+        credentialsProp = UserCredentialsReader.getInstance();
+
+
+        return RestAssured.given()
+                .contentType("multipart/form-data")
+                .multiPart("username", credentialsProp.getProperty("AdminUserName"))
+                .multiPart("password", credentialsProp.getProperty("AdminPassword"))
+                .when()
+                .post("http://127.0.0.1:8000/api/users/login").then()
+                .body("$",hasKey("access"))
+                .extract().path("access");
+    }
 }
