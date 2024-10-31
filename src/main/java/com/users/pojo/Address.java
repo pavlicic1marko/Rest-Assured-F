6f53c6f466402948652384f163826f21ef5d7dd3
MP <pavlicic1marko@gmail.com>
2024-10-31 14:22:01 +0100

    update POJO

--- src/main/java/com/users/pojo/Address.java
+++ src/main/java/com/users/pojo/Address.java
@@ -5,9 +5,46 @@ public class Address {
     public String city;
     public String postalCode;
     public String country;
-    public int order;
+    public int shippingPrice;
 
 
+    public String getAddress() {
+        return address;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getShippingPrice() {
+        return shippingPrice;
+    }
+
+    public void setShippingPrice(int shippingPrice) {
+        this.shippingPrice = shippingPrice;
+    }
 }
--- src/main/java/com/users/pojo/Orders.java
+++ src/main/java/com/users/pojo/Orders.java
@@ -1,5 +1,13 @@
 package com.users.pojo;
 
+import java.util.List;
+
 public class Orders {
     private Address shippingAddress ;
+    private List<Product>  orderItems;
+    private String paymentMethod= "PayPal";
+    private String itemsPrice= "29.99";
+    private String shippingPrice= "10.00";
+    private String taxPrice= "2.46";
+    private String totalPrice= "42.45";
 }
